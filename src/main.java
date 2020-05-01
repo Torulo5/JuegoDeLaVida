570b6159e69f187880114c36d60c6519dd48f65f
Torulo <torulo@hotmail.com>
2020-05-02 00:54:18 +0200

    prueba doble frame

--- src/main.java
+++ src/main.java
@@ -17,23 +17,12 @@ public class main {
 		gFrame.setSize(300,300); 
 		gFrame.setVisible(true);
 		
-		
-		
-//		JFrame frame = new JFrame("Flow Layout");
-//		JButton button,button1, button2, button3,button4;
-//		button = new JButton("button 1");
-//		button1 = new JButton("button 2");
-//		button2 = new JButton("button 3");
-//		button3 = new JButton("button 4");
-//		button4 = new JButton("button 5");
-//		frame.add(button);
-//		frame.add(button1);
-//		frame.add(button2);
-//		frame.add(button3);
-//		frame.add(button4);
-//		frame.setLayout(new GridLayout(2,3));
-//		frame.setSize(300,300);  
-//		frame.setVisible(true);
+//		GameFrame gFrame1 = new GameFrame(gController);
+//		gFrame1.pack();
+//		gFrame1.setSize(300,300); 
+//		gFrame1.setVisible(true);
+//		gController.addNextStatelisteners(gFrame1);
+
 	}
 
 }
